e997cf132aa95bdd573d2d5df2c422418201805b
ontime consultants <ontime.c23@gmail.com>
2023-03-24 21:35:35 +0300

    few changes

--- server/server.js
+++ server/server.js
@@ -26,6 +26,6 @@ app.get('/', (req,res)=> {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`server`))
+// app.listen(PORT, ()=> console.log(`server`))
 
-// mongoose.connect(process.env.DATABASE_CONNECTION_URL,{ useUnifiedTopology: true, useNewUrlParser: true}).then(() => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${ PORT }/api/rcd`))).catch((e) => console.log(e));
+mongoose.connect(process.env.DATABASE_CONNECTION_URL,{ useUnifiedTopology: true, useNewUrlParser: true}).then(() => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${ PORT }/api/rcd`))).catch((e) => console.log(e));
