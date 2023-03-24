e042cae11a6204f7dc7b0723200b0262bb0a6bf9
ontime consultants <ontime.c23@gmail.com>
2023-03-24 21:05:35 +0300

    testing

--- server/server.js
+++ server/server.js
@@ -26,4 +26,6 @@ app.get('/', (req,res)=> {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.DATABASE_CONNECTION_URL,{ useUnifiedTopology: true, useNewUrlParser: true}).then(() => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}/api/rcd`))).catch((e) => console.log(e));
+app.listen(PORT, ()=> console.log(`server`))
+
+// mongoose.connect(process.env.DATABASE_CONNECTION_URL,{ useUnifiedTopology: true, useNewUrlParser: true}).then(() => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${ PORT }/api/rcd`))).catch((e) => console.log(e));
