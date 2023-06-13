5dcef5089770d24349c1e95289b1aed23632b5c4
oc2018 <ontime.c23@gmail.com>
2023-06-13 16:16:46 +0300

    new file:   server/middleware/deleteValidation.js 	modified:   server/routes/pickingRoutes.js

---
+++ server/middleware/deleteValidation.js
@@ -0,0 +1,12 @@
+export const deleteValidation = (req, res, next) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).send({ message: 'Id is required'})
+    }
+     else {
+        alert('Sure you want to delete this Entry?');
+    }
+
+    next()
+}
