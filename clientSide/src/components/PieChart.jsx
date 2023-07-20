102872ab5955513b851b22bdbd3bccd2b46a852a
oc2018 <ontime.c23@gmail.com>
2023-07-20 10:12:06 +0300

    modified:   clientSide/src/components/PieChart.jsx 	modified:   clientSide/package-lock.json 	modified:   clientSide/package.json 	modified:   clientSide/public/index.html 	modified:   clientSide/src/App.jsx 	modified:   clientSide/src/actions/analysis.js 	modified:   clientSide/src/api/index.js 	modified:   clientSide/src/components/PickingsAnalysis.jsx 	modified:   clientSide/src/pages/Dashboard.jsx 	modified:   clientSide/src/pages/ExpenseDetails.jsx 	modified:   clientSide/src/pages/PickingDetails.jsx 	modified:   server/controllers/picking.js 	modified:   server/logs/reqLogs.txt 	modified:   server/middleware/auth.js

--- clientSide/src/components/PieChart.jsx
+++ clientSide/src/components/PieChart.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import ReactApexChart from 'react-apexcharts';
 import { Box, Typography, Stack } from '@mui/material';
 
-const PieChart = ({ title, value, series, colors }) => {
+const PieChart = ({ picks, title, value, series, colors }) => {
   return (
     <Box pl={3.5} position={'relative'}>
-            <Typography fontSize={14} color='#808191'>{ title }</Typography>
-        <Stack direction='column' display={'flex'} justifyContent={'center'} alignItems={'center'}>
-            {/* <Typography  position={'absolute'} top={'50%'} left={'50%'} fontSize={24} color='#11142d' fontWeight={700} >{ value }</Typography> */}
-            <p className={`absolute top-[50%] translate-y-[-25%] text-2xl font-[700] ${ title ==='Expenses' ? `text-[#ff7a63]` : 'text-[#a4f264]' }`}>{ value }</p>
-            <ReactApexChart 
-                options={{
-                    chart: { type: 'donut'},
-                    colors,
-                    legend: { show: false },
-                    dataLabels: { enabled: false },
-                }}
-                series={series}
-                type='donut'
-                width='220px'
-            />
-        </Stack>
+      <Typography marginBottom='10px' fontSize={14} color='#808191'>{ title }</Typography>
+      <Stack direction='column' display={'flex'} justifyContent={'center'} alignItems={'center'}>
+        {/* <Typography  position={'absolute'} top={'50%'} left={'50%'} fontSize={24} color='#11142d' fontWeight={700} >{ value }</Typography> */}
+        <p className={`absolute top-[50%] translate-y-[-25%] text-xl font-[700] ${ title ==='Expenses' ? `text-[#ff7a63]` : 'text-[#a4f264]' }`}>{ value }</p>
+        <ReactApexChart 
+            options={{
+              chart: { type: 'donut'},
+              colors,
+              legend: { show: false },
+              dataLabels: { enabled: false },
+              labels: ['Income', 'Expenses'],
+            }}
+            series={ series }                
+            type='donut'
+            width='220px'
+        />
+      </Stack>
     </Box>
   )
 }
